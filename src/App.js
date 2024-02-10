fba4dcff4be2c46b5be4653ee851b75264a1ed66
Anubhav Pal <anubhavpal15june@gmail.com>
2024-02-11 00:12:47 +0530

    Add Session

--- src/App.js
+++ src/App.js
@@ -2,9 +2,54 @@ import './App.css';
 import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from './components/Auth/login';
 import LoggedUserOrders from './components/LoggedUserOrders';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const { user, isAuthenticated, isLoading } = useAuth0();
+  const { user, isAuthenticated, isLoading, getAccessTokenSilently } = useAuth0();
+
+  const [accessToken, setAccessToken] = useState(null);
+  
+  useEffect(() => {
+    const restoreAuth = async () => {
+      try {
+        const storedAccessToken = localStorage.getItem('accessToken');
+        if (storedAccessToken) {
+          setAccessToken(storedAccessToken);
+        }
+      } catch (error) {
+        console.error('Error restoring authentication:', error);
+      }
+    };
+
+    restoreAuth();
+  }, []);
+
+  useEffect(() => {
+    const saveAuth = () => {
+      if (accessToken) {
+        localStorage.setItem('accessToken', accessToken);
+      } else {
+        localStorage.removeItem('accessToken');
+      }
+    };
+
+    saveAuth();
+  }, [accessToken]);
+
+  useEffect(() => {
+    const fetchAccessToken = async () => {
+      try {
+        const token = await getAccessTokenSilently();
+        setAccessToken(token);
+      } catch (error) {
+        console.error('Error fetching access token:', error);
+      }
+    };
+
+    if (isAuthenticated) {
+      fetchAccessToken();
+    }
+  }, [isAuthenticated, getAccessTokenSilently]);
 
   if (isLoading) {
     return <div className='flex items-center justify-center font-bold text-2xl'>Loading...</div>;
@@ -14,11 +59,13 @@ function App() {
     <div className='overflow-x-hidden'>
       {isAuthenticated ? (
         <>
+          {console.log(accessToken)}
+          {accessToken}
           <LoggedUserOrders />
         </>
       ) : (
         <div className="flex flex-col items-center justify-between gap-5 p-4">
-          <div className='font-bold text-2xl text-center sm:text-start'>Hey there, please login to see your orders</div>
+          <div className='font-bold text-2xl'>Hey there, please login to see your orders</div>
           <LoginButton />
         </div>
       )}
